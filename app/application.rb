20261e4a4bf0bd14fc129656dbdc09423c36d879
Nick Lombardi <nelom3@gmail.com>
2021-06-20 18:22:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,6 @@ class Application
     if req.path.match(/items/)
       item = req.path.split(/items/).last
       
-      binding.pry
       if @@items.include?(item)
         resp.write item.price
       else
