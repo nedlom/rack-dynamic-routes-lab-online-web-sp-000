cb6b25738caf4ae5c06d8a374850cea838f66643
Nick Lombardi <nelom3@gmail.com>
2021-06-20 18:19:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       if @@items.include?(item)
         resp.write item.price
       else
-        resp.write
+        resp.write "Item not found"
         resp.status = 400
     else
       resp.write "Route not found"
