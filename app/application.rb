3dec1ea39524feb4c94ae401691ec4db3af51193
Nick Lombardi <nelom3@gmail.com>
2021-06-20 18:12:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,11 +3,11 @@ class Application
   def call(env)
     
     resp = Rack::Response.new
-    req = Rack::Request.new
+    req = Rack::Request.new(env)
     
     if req.path.match(/item/)
     else
-      
+      resp.status = 404
     end
     
   end
